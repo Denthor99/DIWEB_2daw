554d8c3df264f0b5805e69eba15d5cfa2cfd75e8
Den <drodsan2708@g.educaand.es>
2024-02-07 20:39:32 +0100

    Archivos pendientes

---
+++ Pruebas diw_MiguelSalvado/logica.js
@@ -0,0 +1,25 @@
+function ocultarMenu() {
+    var menuDesplegable = document.getElementById("menuDesplegable");
+    var ocultarSpans = document.getElementById("clickDesplegarMenu");
+  
+    if (menuDesplegable.style.display !== "none") {
+      menuDesplegable.style.display = "none";
+      ocultarSpans.style.display = "flex";
+    }
+  }
+  
+  function desplegarMenu() {
+    var menuDesplegable = document.getElementById("menuDesplegable");
+    var ocultarSpans = document.getElementById("clickDesplegarMenu");
+  
+    if (menuDesplegable.style.display === "none") {
+      menuDesplegable.style.display = "block";
+      ocultarSpans.style.display = "none";
+    }
+  }
+  
+  document.getElementById("menuDesplegable").addEventListener("mouseleave", ocultarMenu);
+  
+  document.getElementById("clickDesplegarMenu").addEventListener("click", desplegarMenu);
+
+  
