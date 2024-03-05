305cbc286fcbc3fbaa8f833ec06d625c09ec1576
Denthor99 <darancuga@hotmail.com>
2024-03-05 14:57:46 +0100

    Menú acordeon

--- Tema 7/AccesibilidadForm/src/assets/scripts/main.js
+++ Tema 7/AccesibilidadForm/src/assets/scripts/main.js
@@ -1,29 +1,38 @@
-
-
-
-
-
 function onIniciar() {
   console.log("Hola Mundo");
-
+ 
   // Conseguir los elementos HTML implicados
   const btnTab = document.querySelectorAll(".botonesTab__pestana");
   const blockText = document.querySelectorAll(".contenedorTexto__bloque");
-
+ 
   // Añadir EventListener a todos los botones
-  btnTab.forEach((boton,i) => {
-    boton.addEventListener("click",()=>{
-      // Quitamos a todos los botones la clase active
-      btnTab.forEach((botonFE,j) => {
-        botonFE.classList.remove("active");
-        blockText[j].classList.remove("active")
-      })
-
-      boton.classList.add("active");
-      blockText[i].classList.add("active");
-    })
+  btnTab.forEach((boton, i) => {
+     boton.addEventListener("click", () => {
+       // Quitamos a todos los botones la clase active
+       btnTab.forEach((botonFE, j) => {
+         botonFE.classList.remove("active");
+         blockText[j].classList.remove("active");
+       });
+ 
+       boton.classList.add("active");
+       blockText[i].classList.add("active");
+     });
   });
-}
-
-
-window.onload = onIniciar();
+ 
+  // Código del acordeon
+  const h2Titulo = document.querySelectorAll(".divContenedor__titulo");
+  const pContenido = document.querySelectorAll(".divContenedor__contenido");
+ 
+  h2Titulo.forEach((nodo, i) => {
+     nodo.addEventListener('click', () => {
+       if (pContenido[i].classList.contains('divContenedor__contenido--activo')) {
+         pContenido[i].classList.remove('divContenedor__contenido--activo');
+       } else {
+         pContenido[i].classList.add('divContenedor__contenido--activo');
+       }
+     });
+  });
+ }
+ 
+ window.onload = onIniciar; 
+ 
